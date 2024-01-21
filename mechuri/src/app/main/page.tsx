c7cdfeb0420a731c59d62664a442dca19af27b39
이지헌 <jiheon_114@naver.com>
2024-01-22 02:20:53 +0900

    feat:NavBar 스켈레톤 코드 구현

--- mechuri/src/app/main/page.tsx
+++ mechuri/src/app/main/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import NavBar  from '@/components/commons/NavBar'
 export default function Page() {
     return (
         <div>
+            <NavBar/>
             메인페이지입니다.
         </div>
     );
