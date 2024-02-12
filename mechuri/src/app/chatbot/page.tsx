25d8ed2f380c9e5c7a31ee70783740abb5e9bade
이지헌 <jiheon_114@naver.com>
2024-02-12 13:44:19 +0900

    chore: 챗봇 기능 구현을 위한 페이지 생성

---
+++ mechuri/src/app/chatbot/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '@/components/commons/NavBar';
+export default function Page() {
+  return (
+    <div>
+      <NavBar />
+      챗봇페이지입니다.
+    </div>
+  );
+}
