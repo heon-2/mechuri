ad7545611340cf066275d9ca418ce8d3bab5d36e
이지헌 <jiheon_114@naver.com>
2024-02-17 19:13:06 +0900

    feat: 선호 음식 테스트 기능 초기 구현

---
+++ mechuri/src/app/foodchoice/FoodTest.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+export default function FoodTest() {
+  return (
+    <div className="grid grid-rows-5 grid-cols-2 h-full">
+      <div className="row-span-1 col-span-2 flex items-end justify-center">
+        Q. 질문 리스트를 뭘 해야 할까요 ?
+      </div>
+
+      <div className="row-span-3 flex items-center justify-center">
+        <div className="w-1/2 aspect-square flex flex-col items-center justify-center border border-red-500">
+          <div className="w-56">
+            <img src="/images/떡볶이.jpg" />
+          </div>
+          <div>떡볶이 먹을래?</div>
+        </div>
+      </div>
+      <div className="row-span-3 flex items-center justify-center">
+        <div className="w-1/2 aspect-square flex flex-col items-center justify-center border border-red-500">
+          <div className="w-56">
+            <img src="/images/떡볶이.jpg" />
+          </div>
+          <div>피자 먹을래?</div>
+        </div>
+      </div>
+      <div className="row-span-1 col-span-2 flex flex-col justify-center items-center">
+        <div>진행바</div>
+        <div>1 / 12</div>
+      </div>
+    </div>
+  );
+}
