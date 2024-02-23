5db513421c511cc215a6adcca2bbc53aafdbdcb0
이지헌 <jiheon_114@naver.com>
2024-02-24 02:21:50 +0900

    feat: 메추리봇 컴포넌트 세팅 및 사용자의 기분을 토대로 메뉴를 추천해주는 메추리봇 기능 구현 및 채팅창 UI 기초 구현

--- mechuri/src/app/mechuribot/page.tsx
+++ mechuri/src/app/mechuribot/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '@/components/commons/NavBar';
+import ChatBot from '@/components/main/ChatBot';
+import MechuriBot from './MechuriBot';
 export default function Page() {
   return (
     <div>
       <NavBar />
-      챗봇페이지입니다.
+      <MechuriBot />
     </div>
   );
 }
--- mechuri/src/components/main/ChatBot.tsx
+++ mechuri/src/components/main/ChatBot.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 export default function ChatBot() {
-  return <div className="flex justify-center items-center">챗봇 소개 페이지입니다.</div>;
+  return <div className="flex justify-center items-center">챗봇 페이지입니다.</div>;
 }
