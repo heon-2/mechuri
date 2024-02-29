13c8b2799e835f251400db6d0253e336e6d586c9
최지수 <cjjss11@naver.com>
2024-02-29 15:18:17 +0900

    feat: 선택한 룰렛 카테고리 데이터를 recoil 사용하여 저장

--- mechuri/src/app/roulette/Select.tsx
+++ mechuri/src/app/roulette/Select.tsx
@@ -1,35 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { categoryState } from '@/stores/atoms/categoryState';
 
 export default function Select() {
+  const [category, setCategory] = useRecoilState(categoryState);
+
+  const selectCategory = (newCategory: string) => {
+    setCategory(newCategory);
+  };
+
+  console.log(category);
+
   return (
-    <div className="bg-gray-300 h-96 w-48 grid grid-rows-4 grid-cols-2 flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
+    <div className="bg-gray-300 h-96 w-48 grid grid-rows-3 grid-cols-2 justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('한식')}
+      >
         <p>사진</p>
         <p>한식</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('일식')}
+      >
         <p>사진</p>
         <p>일식</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('양식')}
+      >
         <p>사진</p>
         <p>양식</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('중식')}
+      >
         <p>사진</p>
-        <p>증식</p>
+        <p>중식</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('분식')}
+      >
         <p>사진</p>
         <p>분식</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center"
+        onClick={() => selectCategory('디저트')}
+      >
         <p>사진</p>
-        <p>야식</p>
-      </div>
-      <div className="col-span-2 flex flex-col justify-center items-center">
-        <p>사진</p>
-        <p>패스트푸드</p>
+        <p>디저트</p>
       </div>
     </div>
   );
