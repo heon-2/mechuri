1c5e385940d70d59ea677b0be113e13a4cbfc75e
이지헌 <jiheon_114@naver.com>
2024-03-01 18:14:41 +0900

    feat: 챗봇 메세지 수정, 컴포넌트 배치, 현재 시간 반영

--- mechuri/src/app/mechuribot/page.tsx
+++ mechuri/src/app/mechuribot/page.tsx
@@ -4,7 +4,7 @@ import ChatBot from '@/components/main/ChatBot';
 import MechuriBot from './MechuriBot';
 export default function Page() {
   return (
-    <div>
+    <div className="h-[calc(100vh-4rem)]">
       <NavBar />
       <MechuriBot />
     </div>
