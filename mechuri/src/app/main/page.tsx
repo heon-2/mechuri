126f394f9b2e703b219c1dfa1e545e4ccfbaee59
이지헌 <jiheon_114@naver.com>
2024-03-01 14:19:37 +0900

    style: 메인화면 색상 테스트

--- mechuri/src/app/main/page.tsx
+++ mechuri/src/app/main/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import NavBar from '@/components/commons/NavBar';
 import MainPage from './MainPage';
-import TestNav from '@/components/commons/TestNav';
+import TestNav from '@/components/commons/NavBar';
 export default function Page() {
   return (
-    <div className="">
+    <div className="bg-[#F6F6F6]">
       <NavBar />
       {/* <TestNav /> */}
       <MainPage />
--- mechuri/src/components/commons/NavBar.tsx
+++ mechuri/src/components/commons/NavBar.tsx
@@ -1,6 +1,4 @@
-'use client';
 import React from 'react';
-
 export default function NavBar() {
   const components: { title: string; href: string; description: string }[] = [
     {
@@ -33,41 +31,15 @@ export default function NavBar() {
   ];
 
   return (
-    // <div className="navbar bg-base-100">
-    //   <div className="flex-1">
-    //     <a className="btn btn-ghost text-xl">daisyUI</a>
-    //   </div>
-    //   <div className="flex-none">
-    //     <ul className="menu menu-horizontal px-1">
-    //       <li>
-    //         <a>Link</a>
-    //       </li>
-    //       <li>
-    //         <details>
-    //           <summary>Parent</summary>
-    //           <ul className="p-2 bg-base-100 rounded-t-none">
-    //             <li>
-    //               <a>Link 1</a>
-    //             </li>
-    //             <li>
-    //               <a>Link 2</a>
-    //             </li>
-    //           </ul>
-    //         </details>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>
-
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-[#CCD3CA] border-gray-200 dark:bg-gray-900 w-full h-16">
+      <div className="flex items-center h-full justify-between mx-20">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8"
             alt="Flowbite Logo"
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#f6f6f6]">
             Mechuri
           </span>
         </a>
@@ -96,12 +68,12 @@ export default function NavBar() {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:border-gray-700">
             {components.map((component, index) => (
               <li key={component.href}>
                 <a
                   href={component.href}
-                  className={`block py-2 px-3 rounded md:p-0 dark:text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent ${
+                  className={`block py-2 px-3 rounded md:p-0 text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent ${
                     index === 0
                       ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500'
                       : 'text-gray-900 md:hover:text-blue-700 md:dark:hover:text-blue-500'
