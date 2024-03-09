34647f1ce9fa76f6d9908f2528ae249e28925655
이지헌 <jiheon_114@naver.com>
2024-03-09 19:47:41 +0900

    refactor: 리액트쿼리 v5 도입 및 로딩 중 UI 띄우기

--- mechuri/src/app/foodchoice/FoodTest.tsx
+++ mechuri/src/app/foodchoice/FoodTest.tsx
@@ -18,17 +18,18 @@ interface AnswerDetail {
 
 import React, { useEffect } from 'react';
 import Link from 'next/link';
+import LoadingUi from '@/components/commons/LoadingUi';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import BottomProgressiveBar from '@/components/foodchoice/BottomProgressiveBar';
+import { useQuery } from '@tanstack/react-query';
 export default function FoodTest() {
   let [step, setStep] = useState(1);
   const [answers, setAnswers] = useState<
     Array<{ step: number; shortQuestion: string; shortAnswer: string }>
   >([]);
-  const [questionnaire, setQuestionnaire] = useState<Questionnaire[]>([]);
-  const totalStep = questionnaire.length; // 전체 질문 개수.
-  const router = useRouter();
+  // const [questionnaire, setQuestionnaire] = useState<Questionnaire[]>([]);
+
   const sendAnswers = async () => {
     const response = await fetch('/api/foodchoice', {
       method: 'POST',
@@ -44,26 +45,46 @@ export default function FoodTest() {
     }
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/foodchoice');
-        if (!response.ok) {
-          throw new Error('Data could not be fetched!');
-        }
-        const data: Questionnaire[] = await response.json(); // 응답을 FoodChoice[] 타입으로 가정
-        setQuestionnaire(data); // 응답 데이터를 상태에 저장
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('/api/foodchoice');
+  //       if (!response.ok) {
+  //         throw new Error('Data could not be fetched!');
+  //       }
+  //       const data: Questionnaire[] = await response.json(); // 응답을 FoodChoice[] 타입으로 가정
+  //       setQuestionnaire(data); // 응답 데이터를 상태에 저장
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
+  // React-Query Test Code
+  const fetchData = async () => {
+    const response = await fetch('/api/foodchoice');
+    if (!response.ok) {
+      throw new Error('에러가 발생했습니다.');
+    }
+    return await response.json(); // 이 부분이 중요
+  };
+  const {
+    data: questionnaire,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useQuery<Questionnaire[]>({
+    queryKey: ['foodchoice'],
+    queryFn: fetchData,
+  });
+
+  const totalStep = questionnaire?.length ?? 0; // 전체 질문 개수.
+  const router = useRouter();
   useEffect(() => {
     // 모든 질문에 대한 답변이 완료되었을 때만 서버로 데이터를 전송
-    if (answers.length === questionnaire.length && questionnaire.length > 0) {
+    if (answers.length === questionnaire?.length && questionnaire.length > 0) {
       sendAnswers();
     }
   }, [answers]);
@@ -74,57 +95,63 @@ export default function FoodTest() {
     );
     if (selectedAnswer) {
       const newAnswer = {
-        step: currentQuestion.step,
-        shortQuestion: currentQuestion.Question.shortQuestion,
+        step: currentQuestion?.step ?? 0,
+        shortQuestion: currentQuestion?.Question?.shortQuestion ?? '',
         shortAnswer: selectedAnswer.shortAnswer,
       };
       setAnswers((prevAnswers) => [...prevAnswers, newAnswer]);
     }
-    if (step < questionnaire.length) {
+    if (step < (questionnaire?.length ?? 0)) {
       setStep((currentStep) => currentStep + 1);
     }
   };
-  const currentQuestion = questionnaire[step - 1];
+  const currentQuestion = questionnaire?.[step - 1];
   return (
     // calc을 사용한 높이 조절 -> 3rem은 NavBar의 높이
     <div className="grid grid-rows-5 w-screen h-[calc(100%-4rem)] bg-[#f6f6f6]">
-      {/* 질문 표시 */}
-      {currentQuestion && (
-        <div className="row-span-1 flex items-center justify-center w-screen font-bold text-5xl text-text">
-          Q{step}. {currentQuestion?.Question?.longQuestion}
-        </div>
-      )}
-
-      {/* 선택지 */}
-      <div className="row-span-3 flex items-center justify-center">
-        <div
-          className={`flex w-3/5 ${currentQuestion?.Answers?.length > 3 ? 'h-1/2' : 'h-4/5'} justify-around `}
-        >
-          {currentQuestion?.Answers?.map((answer, idx) => (
-            <div
-              key={idx}
-              // 추후에 그림자를 뉴모피즘으로 변환예정.
-              className="flex  justify-center items-center cursor-pointer   hover:scale-105 transition-all duration-300 ease-in-out bg-white rounded-2xl shadow-2xl"
-              style={{ flexBasis: `${80 / currentQuestion?.Answers?.length}%` }}
-              onClick={() => handleClick(answer.answerId)}
-            >
-              <div className="h-full w-full flex flex-col items-center justify-center gap-10">
-                {/* 선택지 내용 */}
-                {answer.image && (
-                  <img
-                    className="rounded-xl h-2/3 aspect-[4/3]"
-                    src={answer.image}
-                    alt="Sample Image"
-                  />
-                )}
-
-                <div className="font-semibold text-4xl text-gray-800">{answer.longAnswer}</div>
-              </div>
+      {isLoading ? (
+        <LoadingUi />
+      ) : (
+        <>
+          {/* 질문 표시 */}
+          {currentQuestion && (
+            <div className="row-span-1 flex items-center justify-center w-screen font-bold text-5xl text-text">
+              Q{step}. {currentQuestion?.Question?.longQuestion}
             </div>
-          ))}
-        </div>
-      </div>
-      <BottomProgressiveBar step={step} totalStep={totalStep} />
+          )}
+
+          {/* 선택지 */}
+          <div className="row-span-3 flex items-center justify-center">
+            <div
+              className={`flex w-3/5 ${currentQuestion?.Answers?.length!! > 3 ? 'h-1/2' : 'h-4/5'} justify-around `}
+            >
+              {currentQuestion?.Answers?.map((answer, idx) => (
+                <div
+                  key={idx}
+                  // 추후에 그림자를 뉴모피즘으로 변환예정.
+                  className="flex  justify-center items-center cursor-pointer   hover:scale-105 transition-all duration-300 ease-in-out bg-white rounded-2xl shadow-2xl"
+                  style={{ flexBasis: `${80 / currentQuestion?.Answers?.length}%` }}
+                  onClick={() => handleClick(answer.answerId)}
+                >
+                  <div className="h-full w-full flex flex-col items-center justify-center gap-10">
+                    {/* 선택지 내용 */}
+                    {answer.image && (
+                      <img
+                        className="rounded-xl h-2/3 aspect-[4/3]"
+                        src={answer.image}
+                        alt="Sample Image"
+                      />
+                    )}
+
+                    <div className="font-semibold text-4xl text-gray-800">{answer.longAnswer}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <BottomProgressiveBar step={step} totalStep={totalStep} />
+        </>
+      )}
     </div>
   );
 }
